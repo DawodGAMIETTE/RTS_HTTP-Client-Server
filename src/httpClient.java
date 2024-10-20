21857d2aff2a384ed4d76e390d1db0a397dfb352
DawodGAMIETTE <david.gamiette@gmail.com>
2024-10-20 20:20:55 +0200

    end of the part 2.1

--- src/httpClient.java
+++ src/httpClient.java
@@ -1,5 +1,7 @@
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.Socket;
@@ -8,11 +10,10 @@ import java.net.URL;
 public class httpClient {
     String protocol, host, filename;
     int port;
-    public static void main(String[] args) throws IOException {
 
-        String firstElement = args[0];
+    public static void main(String[] args) throws IOException, InterruptedException {
         httpClient myClient = new httpClient();
-        myClient.readURL(firstElement);
+        myClient.readURL(args[0]);
         myClient.getURL();
     }
 
@@ -20,17 +21,40 @@ public class httpClient {
         URL url = new URL(in);
         protocol = url.getProtocol();
         host = url.getHost();
-
         filename = "/rfc.txt";
         port = 80;
     }
 
-    public void getURL() throws IOException {
+    public void getURL() throws IOException, InterruptedException {
         Socket socket = new Socket(host, port);
-        while(true) {
+        System.out.println("Connected to " + host + " on port " + port);
+        if (socket.isConnected() && !socket.isClosed()) {
+            Thread.sleep(5000);
             System.out.println("Waiting for the client request");
-            InputStream inputStream = socket.getInputStream();
-            PrintWriter outputStream = new PrintWriter(socket.getOutputStream(), true);
+
+            // Send the HTTP GET command to the web server
+            PrintWriter to = new PrintWriter(socket.getOutputStream(), true);
+            to.println("GET " + filename + " HTTP/1.1");
+            to.println("Host: " + host);
+            to.println("User-Agent: @DawodGAMIETTE");
+
+            // Create the output file locally
+            OutputStream fileOutputStream = new FileOutputStream(filename);
+
+            // Read the server response from the socket and write it to the file
+            InputStream from = socket.getInputStream();
+            byte[] buf = new byte[4096];
+            int bytes_read;
+            while ((bytes_read = from.read(buf)) != -1) {
+                fileOutputStream.write(buf, 0, bytes_read);
+            }
+
+            from.close();
+            fileOutputStream.close();
+            to.close();
+            socket.close();
+        } else {
+            System.out.println("Socket is not open.");
         }
     }
 }
